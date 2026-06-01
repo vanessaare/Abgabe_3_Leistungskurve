7b3ee6a8e378ef165e674e79669696af869ad331
Vanessa Reich <rv3725@mci4me.at>
2026-06-01 12:04:12 +0200

    deleted alternitiv_calc

--- calc.py
+++ calc.py
@@ -1,12 +1,12 @@
 import pandas as pd
 
-from clean_data import lade_df_activity, entferne_leere_werte
+from clean_data import lade_df_activity, clean_df_activity
  
  
 def berechne_power_curve(val_col="PowerOriginal", intervals=[5, 10, 20, 60, 300, 1800]):
     results = []
     df_activity = lade_df_activity()
-    df_activity = entferne_leere_werte(df_activity)
+    df_activity = clean_df_activity(df_activity)
     for seconds in intervals:
         if len(df_activity) >= seconds:
             max_avg = df_activity[val_col].fillna(0).rolling(window=seconds).mean().max()
