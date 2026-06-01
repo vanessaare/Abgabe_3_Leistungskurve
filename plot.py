d16a5c128add093e6d76c2de1ee4c9e05f8f29d8
P-M16 <pm9441@mci4me.at>
2026-06-01 10:59:55 +0200

    working graph

--- plot.py
+++ plot.py
@@ -1,25 +1,7 @@
 import matplotlib.pyplot as plt
 
-from clean_data import lade_df_activity
-from calc import berechne_power_curve
 
-
-def plot_power_curve(zeitaufloesung=1):
-    """
-    Plottet die Power Curve.
-    """
-
-    # 1. Daten laden
-    df = lade_df_activity()
-
-    power = df["PowerOriginal"]
-
-    # 2. Power Curve berechnen
-    curve_df = berechne_power_curve(
-        power=power, zeitaufloesung=zeitaufloesung
-    )
-
-    # 3. Plot
+def plot_power_curve(curve_df):
     plt.figure(figsize=(10, 5))
 
     plt.plot(
@@ -27,15 +9,19 @@ def plot_power_curve(zeitaufloesung=1):
         curve_df["Leistung_W"],
         linewidth=2
     )
+
+    plt.xscale("log")
+    plt.xlim(1, 1800)
+
     ticks = [5, 10, 20, 60, 300, 1800]
     labels = ["5 s", "10 s", "20 s", "1 min", "5 min", "30 min"]
 
     plt.xticks(ticks, labels)
-    plt.xlabel("Zeit [s]")
+
+    plt.xlabel("Zeit")
     plt.ylabel("Leistung [W]")
     plt.title("Power Curve")
     plt.grid(True)
 
     plt.tight_layout()
     plt.show()
-
