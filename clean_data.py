a84bcea1db68882edf65be484f3c5c88118c6792
Vanessa Reich <rv3725@mci4me.at>
2026-06-01 11:44:53 +0200

    clean_data verbessert

--- clean_data.py
+++ clean_data.py
@@ -1,32 +1,8 @@
 import pandas as pd
 
 def lade_df_activity():
-    """
-    Lädt activity.csv-Datei
-    """
-<<<<<<< HEAD
-
-    df = pd.read_csv("data/activity.csv", skipinitialspace=True)
+    return pd.read_csv("data/activity.csv", skipinitialspace=True)
 
 
-
-def clean_df_activity():
-    df = df.dropna(subset=["PowerOriginal"])
-
-    df["Time"] = range(len(df))
-
-    return df
-=======
-    df_activity = pd.read_csv("data/activity.csv", skipinitialspace=True)
-    df_activity["Time"] = df_activity.index
-    return df_activity
-
-
-def entferne_leere_werte(df):
-    """
-    Entfernt alle Zeilen ohne PowerOriginal
-    """
-    df_activity = df.dropna(subset=["PowerOriginal"])
-    return df_activity
->>>>>>> c8710e113832e3108c07ce617a1078c4f2009bd0
-    
+def clean_df_activity(df_activity):
+    return df_activity.dropna(subset=["PowerOriginal"])
